44e941983188daf596e7642b1accb5e489cd9f0b
Kabby <1925023+tehkab@users.noreply.github.com>
2023-07-25 19:40:10 -0400

    Initial commit, working without configuration

---
+++ Patch_Withdrawal.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using Verse;
+using RimWorld;
+
+namespace TweakWithdrawalEmergencies
+{
+    [HarmonyPatch(typeof(Alert_LifeThreateningHediff), "get_SickPawns")]
+    static class Patch_Withdrawal
+    {
+        //TODO what type is hd really?
+        private static bool hasLifeThreateningHediffNotOfType(Pawn p, Object hd)
+        {
+            foreach(Hediff h in p.health.hediffSet.hediffs)
+            {
+                if(h != null && h.def != hd && h.CurStage != null && h.CurStage.lifeThreatening && !h.FullyImmune())
+                    return true;
+            }
+
+            return false;
+        }
+
+        private static List<Hediff> getDrugDependencyHediffsFor(Pawn p)
+        {
+            List<Hediff> ret = null;
+
+            foreach(Hediff h in p.health.hediffSet.hediffs)
+            {
+                if(h.def == HediffDefOf.GeneticDrugNeed)
+                {
+                    if(ret == null)
+                        ret = new List<Hediff>();
+                    
+                    ret.Add(h);
+                }
+            }
+
+            return ret;
+        }
+
+        static void Postfix(ref List<Pawn> __result)
+        {
+            if (__result.Count <= 0)
+                return;
+
+            List<Pawn> ret_SickPawns = null;
+
+            double threshold = 11.0; //TODO configurable!
+
+            foreach(Pawn p in __result)
+            {
+                List<Hediff> drugDependencies = getDrugDependencyHediffsFor(p);
+                
+                //no drug dependencies, ignore
+                if(drugDependencies == null)
+                {
+                    continue;
+                }
+
+                //ignore if anything else life threatening
+                if(hasLifeThreateningHediffNotOfType(p, HediffDefOf.GeneticDrugNeed))
+                {
+                    continue;
+                }
+
+                //test drug dependencies (could be multiple)
+                bool over_threshold = false;
+                foreach(Hediff h in drugDependencies)
+                {
+                    if(h.Severity > threshold)
+                    {
+                        over_threshold = true;
+                    }
+                }
+
+                //ignore if no drug dependency hediff is over our set threshold
+                if(over_threshold)
+                {
+                    continue;
+                }
+                
+                //exclude this pawn from medical emergency alerts
+                if(ret_SickPawns == null)
+                {
+                    ret_SickPawns = __result.ListFullCopy();
+                }
+                ret_SickPawns.Remove(p);
+            }
+
+            if(ret_SickPawns != null)
+            {
+                __result = ret_SickPawns;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+## Tweak Withdrawal Emergencies
+
+A Rimworld mod that allows adjusting the Medical Emergency alert from gene-based chemical dependencies.
+In the base game, this happens when the pawn goes comatose, which is still thirty(!) days from death.
+This mod allows adjusting that threshold down as close to death as you want, or even disable it entirely!
+
+### TODO list
+
+The threshold can be customized separately for colonists or prisoners.
+
