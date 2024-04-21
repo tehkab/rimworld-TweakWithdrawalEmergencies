0c562f4c4d13fa3bd5555593a4f0a26b4cc2d510
Kabby <1925023+tehkab@users.noreply.github.com>
2024-04-21 18:22:54 -0400

    remove unused buildscript

--- build-linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -rf mod-build
-mkdir -p mod-build/Assemblies
-FrameworkPathOverride="/usr/lib/mono/4.7.2-api/" dotnet build TweakWithdrawalEmergencies.csproj /property:Configuration=Release
-cp bin/Release/TweakWithdrawalEmergencies.dll mod-build/Assemblies
-cp -r ./About mod-build/About
-cp -r ./Languages mod-build/Languages
